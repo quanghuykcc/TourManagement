d1942179f96fe2c6b4f946f5cc9328f865c28155
levietnhut <levietnhutit@gmail.com>
2016-05-12 11:04:39 +0700

    commit

--- admincp/login.php
+++ admincp/login.php
@@ -7,12 +7,13 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/www/tour/autoload_admin.php';
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta charset="utf-8">
-		<title>Bootstrap Login Form</title>
+		<title>Login Page</title>
 		<meta name="generator" content="Bootply" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<link rel="shortcut icon" href="/www/tour/files/iconn.png">
 		<!--[if lt IE 9]>
 			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
